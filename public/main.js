713ff03c2418687bdcc78273db921150c0cbf54e
Jorge Castillo <ren_shadow_22@hotmail.com>
2016-09-22 20:16:40 -0700

    clase  22-09-2016
    clase del dia

--- public/main.js
+++ public/main.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
     $('#divSalida').html('listo.');
     $('#imgtaric').click(function(){
         $('#divSalida').html('un click');
+        $('#divSalida').addClass('normal');
+        $('#divSalida').removeClass('error');
         $.get('/helloworld/',{},function(conte){
-          $('#divSalida').html(''+conte);   
+          $('#divSalida').html(''+conte);          
         });
     });
 $('#imglulu').click(function(){
+            $('#divSalida').addClass('normal');
+            $('#divSalida').removeClass('error');
        //console.log('hola we');
         $.getJSON('/alumnos/',function(datos){
             //console.log(datos);  
@@ -16,7 +20,11 @@ $('#imglulu').click(function(){
                 elemento.push(v.nombre)
             });
             $('#divSalida').html(''+elemento.join('<br>'));
-        });
+        }).fail(function(){
+            $('#divSalida').html('Error');
+            $('#divSalida').addClass('error');
+        });   
+                 
     });
 
 });
